3ea143b9b51ce146c0de411f6f6888be900366bc
Никита Богачев <nikitabogachev@rambler.ru>
2023-04-21 20:21:49 +0300

    homework done

---
+++ Practice/Homework/Homework1/Program.cs
@@ -0,0 +1,78 @@
+﻿// Task 2
+// Console.Write("Input first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// Console.Write("Input second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (max > b)
+// {
+//     Console.WriteLine($"The bigger number is {max}");
+// }
+// else
+// {
+//     Console.WriteLine($"The bigger number is {b}");
+// }
+
+// Task4
+// Console.Write("Input first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// Console.Write("Input second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third number: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (max > b)
+// {
+//     if (max > c)
+//     {
+//         Console.WriteLine($"The biggest number is {max}");
+//     }
+//     else
+//     {
+//         max = c;
+//         Console.WriteLine($"The biggest number is {max}");
+//     }
+// }
+// else
+// {
+//     max = b;
+//     if (max > c)
+//     {
+//         Console.WriteLine($"The biggest number is {max}");
+//     }
+//     else
+//     {
+//         max = c;
+//         Console.WriteLine($"The biggest number is {max}");
+//     }
+// }
+
+// Task6
+// Console.Write("Input a number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a%2 == 0)
+// {
+//     Console.WriteLine($"The number {a} is even");
+// }
+// else
+// {
+//     Console.WriteLine($"The number {a} is odd");
+// }
+
+// Task8
+// Console.Write("Input a number: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int count = 1;
+
+// while (count <= N)
+// {
+//     if (count%2 == 0)
+//     {
+//         Console.Write(count + " ");
+//     }
+//     count += 1;
+// }
