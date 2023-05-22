30c1671f5a447efe582decfa8add11e86def9663
Никита Богачев <nikitabogachev@rambler.ru>
2023-05-22 22:28:01 +0300

    Обновлено домашнее задание 5

--- Practice/Homework/Homework 5/Program.cs
+++ Practice/Homework/Homework 5/Program.cs
@@ -3,45 +3,57 @@
 
 // [345, 897, 568, 234] -> 2
 
-// Console.WriteLine("Введите размерность массива: ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите минимальное значение массива"); // 100
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите максимальное значение массива"); // 999
-// int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+_refresh:
+Console.WriteLine("Введите минимальное значение массива"); // 100
+int min = Convert.ToInt32(Console.ReadLine());
+if (min < 100)
+{
+    Console.WriteLine("Введено некорректное минимальное трехзначное число");
+    goto _refresh;
+}
+_refresh1:
+Console.WriteLine("Введите максимальное значение массива"); // 999
+int max = Convert.ToInt32(Console.ReadLine());
+if (max < min || max > 999)
+{
+    Console.WriteLine("Введено некорректное максимальное трехзначное число");
+    goto _refresh1;
+}
 
-// int[] array = createArray(size, min, max);
-// printArray(array);
-// Console.WriteLine($"Количество чётных чисел в массиве равно {countEvenNumbers(array)}");
+int[] array = createArray(size, min, max);
+printArray(array);
+Console.WriteLine($"Количество чётных чисел в массиве равно {countEvenNumbers(array)}");
 
-// int[] createArray(int size, int min, int max)
-// {
-//     int[] array = new int[size];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         array[i] = new Random().Next(min, max + 1);
-//     }
-//     return array;
-// }
+int[] createArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
 
-// void printArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i]+" ");
-//     }
-//     Console.WriteLine();
-// }
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
 
-// int countEvenNumbers(int[] array)
-// {
-//     int count = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] % 2 == 0) count++;
-//     }
-//     return count;
-// }
+int countEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами.
 // Найдите сумму элементов, стоящих на нечётных позициях.
@@ -94,48 +106,48 @@
 
 // [3 7 22 2 78] -> 76
 
-Console.WriteLine("Введите размерность массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите минимальное значение массива");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите максимальное значение массива");
-int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите размерность массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение массива");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение массива");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-double[] array = createArray(size, min, max);
-printArray(array);
-Console.WriteLine($"{difference(array)} - разница между максимальным и минимальным элементом массива");
+// double[] array = createArray(size, min, max);
+// printArray(array);
+// Console.WriteLine($"{difference(array)} - разница между максимальным и минимальным элементом массива");
 
-double[] createArray(int size, int min, int max)
-{
-    double[] array = new double[size];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(min, max + 1);
-        array[i] += new Random().NextDouble();
-    }
-    return array;
-}
+// double[] createArray(int size, int min, int max)
+// {
+//     double[] array = new double[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//         array[i] += new Random().NextDouble();
+//     }
+//     return array;
+// }
 
-void printArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i]+" ");
-    }
-    Console.WriteLine();
-}
+// void printArray(double[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]+" ");
+//     }
+//     Console.WriteLine();
+// }
 
-double difference(double[] array)
-{
-    double diff;
-    double max = array[0];
-    double min = array[0];
+// double difference(double[] array)
+// {
+//     double diff;
+//     double max = array[0];
+//     double min = array[0];
 
-    for (int i = 1; i < array.Length; i++)
-    {
-        if (max < array[i]) max = array[i];
-        if (min > array[i]) min = array[i];
-    }
-    diff = max - min;
-    return diff;
-}
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if (max < array[i]) max = array[i];
+//         if (min > array[i]) min = array[i];
+//     }
+//     diff = max - min;
+//     return diff;
+// }
