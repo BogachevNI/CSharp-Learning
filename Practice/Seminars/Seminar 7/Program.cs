d7ce0f9dc671a6ba5c55ef56fde7d502eb0f47c7
Никита Богачев <nikitabogachev@rambler.ru>
2023-05-29 21:09:06 +0300

    Обновлен семинар 7

--- Practice/Seminars/Seminar 7/Program.cs
+++ Practice/Seminars/Seminar 7/Program.cs
@@ -93,70 +93,14 @@
 
 // Задача 3.Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
 
-// int[,] create2DRandomArray(int rows, int columns, int minValue, int maxValue)
-// {
-//     int[,] newArray = new int[rows, columns];
-//     for (int i = 0; i < rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             newArray[i, j] = new Random().Next(minValue, maxValue + 1);
-//         }
-//     }
-//     return newArray;
-// }
-
-// void print2DArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write(array[i, j] + " ");
-//         }
-//         Console.WriteLine();
-//     }
-//     // Console.WriteLine(); // для новой строки
-// }
-
-// int[,] Pow2(int[,] array)
-// {
-//     int [,] newArray = array;
-//     for (int i = 0; i < newArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < newArray.GetLength(1); j++)
-//         {
-//             if (i % 2 == 0 && j % 2 == 0) newArray[i,j] = newArray[i,j]*newArray[i,j];
-//         }
-//     }
-//     return newArray;
-// }
-
-// Console.WriteLine("Input array row amount: ");
-// int rows = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input array column amount: ");
-// int columns = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input array minimum: ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input array maximum: ");
-// int max = Convert.ToInt32(Console.ReadLine());
-
-// int[,] myArray = create2DRandomArray(rows, columns, min, max);
-// print2DArray(myArray);
-// Console.WriteLine();
-// int[,] newArray = Pow2(myArray);
-// print2DArray(newArray);
-
-//Задача 2. ****Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
-
-int[,] create2DRandomArray(int rows, int columns/*, int minValue, int maxValue*/)
+int[,] create2DRandomArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, columns];
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
         {
-            newArray[i, j] = i+j;
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return newArray;
@@ -175,14 +119,69 @@ void print2DArray(int[,] array)
     // Console.WriteLine(); // для новой строки
 }
 
+int[,] Pow2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) array[i,j] = array[i,j]*array[i,j];
+        }
+    }
+    return array;
+}
+
 Console.WriteLine("Input array row amount: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input array column amount: ");
 int columns = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input array minimum: ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input array maximum: ");
-// int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input array minimum: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input array maximum: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = create2DRandomArray(rows, columns/*, min, max*/);
+int[,] myArray = create2DRandomArray(rows, columns, min, max);
 print2DArray(myArray);
+Console.WriteLine();
+int[,] newArray = Pow2(myArray);
+print2DArray(newArray);
+
+//Задача 2. ****Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+
+// int[,] create2DRandomArray(int rows, int columns/*, int minValue, int maxValue*/)
+// {
+//     int[,] newArray = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             newArray[i, j] = i+j;
+//         }
+//     }
+//     return newArray;
+// }
+
+// void print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     // Console.WriteLine(); // для новой строки
+// }
+
+// Console.WriteLine("Input array row amount: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input array column amount: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Input array minimum: ");
+// // int min = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Input array maximum: ");
+// // int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = create2DRandomArray(rows, columns/*, min, max*/);
+// print2DArray(myArray);
